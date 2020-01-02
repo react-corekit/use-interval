ab205c7a0b3b4cb2dbe890516eb4e368db146aa9
glongh <glongh@gmail.com>
2020-01-02 14:16:05 -0500

    docs(Example): Update example to demostrate reset and pause behavior.

--- example/src/App.js
+++ example/src/App.js
@@ -3,13 +3,25 @@ import React, { useState } from "react";
 import { useInterval } from "@react-corekit/use-interval";
 
 const App = () => {
+  const [delay] = useState(1000);
   const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
+  useInterval(
+    () => {
+      setCount(count + 1);
+    },
+    isRunning ? delay : null
+  );
 
-  useInterval(() => {
-    setCount(count + 1);
-  }, 1000);
-
-  return <h1>{count}</h1>;
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setIsRunning(!isRunning)}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+    </div>
+  );
 };
 
 export default App;
